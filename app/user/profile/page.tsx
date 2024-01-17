e0314f54e3407222556e49650951b77e90e6ad76
220026 <220026pp@yse-c.net>
2024-01-17 11:22:55 +0900

    update

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,9 +1,17 @@
 "use client"
 
+import Image from "next/image";
+import imageMe from '@/public/images/me.png'
+
 const ProfilePage = () => {
     return (
         <div>
-            Enter
+            <Image 
+            src={imageMe}
+            className="h-36 w-36" 
+            alt=""
+            />
+            <h1 className="text-2xl text-center font-bold">Profile</h1>
         </div>
     );
 }
